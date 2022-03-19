6ecc74fd849f56cfd32ea71f88433aa9577a18ad
wyy <yuyangwu66@163.com>
2022-03-19 08:23:48 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello git-hot-fix");
         System.out.println("master-test");
         System.out.println("hello git-hot-fix-test");
+        System.out.println("push test");
     }
 
 
